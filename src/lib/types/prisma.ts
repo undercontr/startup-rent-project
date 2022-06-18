c8cdaf7fecc5ae80ea83b0a354b50e6f8a2ff493
Önder Alkan <onder.alkan15@gmail.com>
2022-06-19 01:17:53 +0300

    satış listesi ve kiralama onay sistemi kuruluyor

--- src/lib/types/prisma.ts
+++ src/lib/types/prisma.ts
@@ -1,17 +1,22 @@
 import { Prisma } from "@prisma/client"
 
 const carWithRelations = Prisma.validator<Prisma.CarArgs>()({
-    include: {fuelType: true, brand: true, userCars: true}
+    include: { fuelType: true, brand: true, userCars: true }
 })
 
 const userWithRelations = Prisma.validator<Prisma.UserArgs>()({
-    include: {userCars: true, bought: true, sold: true}
+    include: { userCars: true, bought: true, sold: true }
 })
 
 const userCarWithRelations = Prisma.validator<Prisma.UserCarArgs>()({
-    include: {car: {include: {brand: true, fuelType: true}}, user: true}
+    include: { car: { include: { brand: true, fuelType: true } }, user: true }
 })
+
+const salesWithRelations = Prisma.validator<Prisma.SalesArgs>()({
+    include: {userBuyer: true, userSeller: true, userCar: true}
+});
 
 export type CarWithRelations = Prisma.CarGetPayload<typeof carWithRelations>
 export type UserWithRelations = Prisma.UserGetPayload<typeof userWithRelations>
 export type UserCarWithRelations = Prisma.UserCarGetPayload<typeof userCarWithRelations>
+export type SalesWithRelations = Prisma.SalesGetPayload<typeof salesWithRelations>
--- src/pages/account/sold.tsx
+++ src/pages/account/sold.tsx
@@ -1,3 +1,61 @@
-export default function Sold() {
-    return (<div></div>)
-};
+import { PrismaClient } from "@prisma/client";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+import { AppContext } from "next/app";
+import SoldCard from "../../components/PageElements/SoldCard";
+
+export default function Sold({soldCars}) {
+	console.log(soldCars)
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-between items-center">
+        <h1 className="text-5xl font-bold my-3">Satışlarım</h1>
+        <div>
+          <label className="mr-2">Durum</label>
+          <select className="p-2 rounded-md bg-slate-200">
+            <option value="">Onay Bekleyen</option>
+            <option value="">Onaylanan</option>
+            <option value="">Reddedilen</option>
+          </select>
+        </div>
+      </div>
+      <hr />
+      <div className="my-3 grid grid-cols-1 gap-2">
+        {soldCars.map((e) => (
+			<div key={e.id}>asd</div>
+		))}
+
+		<SoldCard />
+		
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  const client = new PrismaClient();
+
+  const soldCars = await client.sales.findMany({
+    where: { userSeller: { email: session?.user.email } },
+    include: {
+      userBuyer: true,
+      userCar: {
+        include: {
+          car: {
+            include: {
+              brand: true,
+              fuelType: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  console.log(soldCars)
+  return {
+    props: {
+      soldCars: soldCars,
+    },
+  };
+}
